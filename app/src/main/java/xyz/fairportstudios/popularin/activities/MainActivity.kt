8d4b7148cda0066123c89188150a45d623149eae
Rommy Gustiawan <rgcppp@gmail.com>
2020-07-05 12:12:28 +0700

    Mengubah lokasi package activity main

--- app/src/main/java/xyz/fairportstudios/popularin/activities/MainActivity.kt
+++ app/src/main/java/xyz/fairportstudios/popularin/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package xyz.fairportstudios.popularin
+package xyz.fairportstudios.popularin.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import xyz.fairportstudios.popularin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
