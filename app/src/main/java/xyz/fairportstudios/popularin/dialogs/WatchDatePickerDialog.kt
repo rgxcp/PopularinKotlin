daaa9ae314f44e8106c4d419b868ee1cc6d2d62e
Rommy Gustiawan <rgcppp@gmail.com>
2020-07-09 06:54:09 +0700

    Membuat kerangka dialog watch date picker

---
+++ app/src/main/java/xyz/fairportstudios/popularin/dialogs/WatchDatePickerDialog.kt
@@ -0,0 +1,11 @@
+package xyz.fairportstudios.popularin.dialogs
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class WatchDatePickerDialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
