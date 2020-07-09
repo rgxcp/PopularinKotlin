ae50c3c14af54ef52c205d88900e147c804613a8
Rommy Gustiawan <rgcppp@gmail.com>
2020-07-09 07:06:51 +0700

    Membuat kerangka fragment

---
+++ app/src/main/java/xyz/fairportstudios/popularin/fragments/WatchlistFromFollowingFragment.kt
@@ -0,0 +1,16 @@
+package xyz.fairportstudios.popularin.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import xyz.fairportstudios.popularin.R
+
+class WatchlistFromFollowingFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(R.layout.activity_main, container, false)
+
+        return view
+    }
+}
