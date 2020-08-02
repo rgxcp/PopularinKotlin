6450564aa9a8162d8f34bac82e35d30c00fd9fd6
Rommy Gustiawan <rgcppp@gmail.com>
2020-08-02 15:46:28 +0700

    Mengisi kerangka activity splash

--- app/src/main/java/xyz/fairportstudios/popularin/activities/SplashActivity.kt
+++ app/src/main/java/xyz/fairportstudios/popularin/activities/SplashActivity.kt
@@ -1,12 +1,21 @@
 package xyz.fairportstudios.popularin.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import xyz.fairportstudios.popularin.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
     }
 }
