6949ddbbf529bc6bdce2e14c78ce68c5c94653db
Rommy Gustiawan <rgcppp@gmail.com>
2020-08-02 11:21:43 +0700

    Mengisi kerangka dialog watch date picker

--- app/src/main/java/xyz/fairportstudios/popularin/dialogs/WatchDatePickerDialog.kt
+++ app/src/main/java/xyz/fairportstudios/popularin/dialogs/WatchDatePickerDialog.kt
@@ -1,11 +1,27 @@
 package xyz.fairportstudios.popularin.dialogs
 
+import android.app.DatePickerDialog
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import java.util.*
 
 class WatchDatePickerDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return super.onCreateDialog(savedInstanceState)
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            requireActivity(),
+            activity as DatePickerDialog.OnDateSetListener,
+            year,
+            month,
+            day
+        )
+        datePickerDialog.datePicker.maxDate = calendar.timeInMillis
+        return datePickerDialog
     }
 }
