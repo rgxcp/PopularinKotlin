71e463410e3887d51f65cf6af7969a558eb3d20f
Rommy Gustiawan <rgcppp@gmail.com>
2020-08-07 19:06:09 +0700

    Mengisi kerangka fragment empty account

--- app/src/main/java/xyz/fairportstudios/popularin/fragments/EmptyAccountFragment.kt
+++ app/src/main/java/xyz/fairportstudios/popularin/fragments/EmptyAccountFragment.kt
@@ -1,16 +1,43 @@
 package xyz.fairportstudios.popularin.fragments
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import xyz.fairportstudios.popularin.R
+import xyz.fairportstudios.popularin.activities.SignInActivity
+import xyz.fairportstudios.popularin.activities.SignUpActivity
 
 class EmptyAccountFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = inflater.inflate(R.layout.activity_main, container, false)
+        val view: View = inflater.inflate(R.layout.reusable_empty_account, container, false)
+
+        // Context
+        val context = requireActivity()
+
+        // Binding
+        val buttonSignIn: Button = view.findViewById(R.id.button_rea_sign_in)
+        val buttonSignUp: Button = view.findViewById(R.id.button_rea_sign_up)
+
+        // Activity
+        buttonSignIn.setOnClickListener { gotoSignIn(context) }
+
+        buttonSignUp.setOnClickListener { gotoSignUp(context) }
 
         return view
+    }
+
+    private fun gotoSignIn(context: Context) {
+        val intent = Intent(context, SignInActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun gotoSignUp(context: Context) {
+        val intent = Intent(context, SignUpActivity::class.java)
+        startActivity(intent)
     }
 }
