c999b2551e9825a49d0701189ac5122c3c516fa1
Rommy Gustiawan <rgcppp@gmail.com>
2020-08-08 15:05:56 +0700

    Mengisi kerangka activity edit profile

--- app/src/main/java/xyz/fairportstudios/popularin/activities/EditProfileActivity.kt
+++ app/src/main/java/xyz/fairportstudios/popularin/activities/EditProfileActivity.kt
@@ -3,10 +3,16 @@ package xyz.fairportstudios.popularin.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import xyz.fairportstudios.popularin.R
+import xyz.fairportstudios.popularin.fragments.EditProfileFragment
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_edit_profile)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_aep_container, EditProfileFragment())
+            .commit()
     }
 }
