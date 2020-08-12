53cdc912fc9a56ba503ae00f656ba1ae20abf787
Rommy Gustiawan <rgcppp@gmail.com>
2020-08-12 18:37:52 +0700

    Mengisi kerangka fragment like from all

--- app/src/main/java/xyz/fairportstudios/popularin/fragments/LikeFromAllFragment.kt
+++ app/src/main/java/xyz/fairportstudios/popularin/fragments/LikeFromAllFragment.kt
@@ -1,16 +1,155 @@
 package xyz.fairportstudios.popularin.fragments
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.material.snackbar.Snackbar
 import xyz.fairportstudios.popularin.R
+import xyz.fairportstudios.popularin.activities.UserDetailActivity
+import xyz.fairportstudios.popularin.adapters.UserAdapter
+import xyz.fairportstudios.popularin.apis.popularin.get.LikeFromAllRequest
+import xyz.fairportstudios.popularin.models.User
+import xyz.fairportstudios.popularin.statics.Popularin
 
-class LikeFromAllFragment : Fragment() {
+class LikeFromAllFragment(private val reviewID: Int) : Fragment(), UserAdapter.OnClickListener {
+    // Variable untuk fitur load more
+    private var mIsLoading: Boolean = true
+    private var mIsLoadFirstTimeSuccess: Boolean = false
+    private val mStartPage: Int = 1
+    private var mCurrentPage: Int = 1
+    private var mTotalPage: Int = 0
+
+    // Variable member
+    private lateinit var mContext: Context
+    private lateinit var mUserList: ArrayList<User>
+    private lateinit var mAnchorLayout: CoordinatorLayout
+    private lateinit var mLikeFromAllRequest: LikeFromAllRequest
+    private lateinit var mProgressBar: ProgressBar
+    private lateinit var mRecyclerUser: RecyclerView
+    private lateinit var mSwipeRefresh: SwipeRefreshLayout
+    private lateinit var mTextMessage: TextView
+    private lateinit var mUserAdapter: UserAdapter
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = inflater.inflate(R.layout.activity_main, container, false)
+        val view: View = inflater.inflate(R.layout.reusable_recycler, container, false)
+
+        // Context
+        mContext = requireActivity()
+
+        // Binding
+        mAnchorLayout = view.findViewById(R.id.anchor_rr_layout)
+        mProgressBar = view.findViewById(R.id.pbr_rr_layout)
+        mRecyclerUser = view.findViewById(R.id.recycler_rr_layout)
+        mSwipeRefresh = view.findViewById(R.id.swipe_refresh_rr_layout)
+        mTextMessage = view.findViewById(R.id.text_rr_message)
+
+        // Mendapatkan data awal
+        mLikeFromAllRequest = LikeFromAllRequest(mContext, reviewID)
+        getLikeFromAll(mStartPage, false)
+
+        // Activity
+        mRecyclerUser.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1) && newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    if (!mIsLoading && mCurrentPage <= mTotalPage) {
+                        mIsLoading = true
+                        mSwipeRefresh.isRefreshing = true
+                        getLikeFromAll(mCurrentPage, false)
+                    }
+                }
+            }
+        })
+
+        mSwipeRefresh.setOnRefreshListener {
+            mIsLoading = true
+            mSwipeRefresh.isRefreshing = true
+            getLikeFromAll(mStartPage, true)
+        }
 
         return view
+    }
+
+    override fun onUserItemClick(position: Int) {
+        val currentItem = mUserList[position]
+        gotoUserDetail(currentItem.id)
+    }
+
+    private fun getLikeFromAll(page: Int, refreshPage: Boolean) {
+        mLikeFromAllRequest.sendRequest(page, object : LikeFromAllRequest.Callback {
+            override fun onSuccess(totalPage: Int, userList: ArrayList<User>) {
+                when (mIsLoadFirstTimeSuccess) {
+                    true -> {
+                        if (refreshPage) {
+                            mCurrentPage = 1
+                            mUserList.clear()
+                            mUserAdapter.notifyDataSetChanged()
+                        }
+                        val insertIndex = mUserList.size
+                        mUserList.addAll(insertIndex, userList)
+                        mUserAdapter.notifyItemRangeInserted(insertIndex, userList.size)
+                        mRecyclerUser.scrollToPosition(insertIndex)
+                    }
+                    false -> {
+                        mUserList = ArrayList()
+                        val insertIndex = mUserList.size
+                        mUserList.addAll(insertIndex, userList)
+                        mUserAdapter = UserAdapter(mContext, mUserList, this@LikeFromAllFragment)
+                        mRecyclerUser.adapter = mUserAdapter
+                        mRecyclerUser.layoutManager = LinearLayoutManager(mContext)
+                        mRecyclerUser.visibility = View.VISIBLE
+                        mProgressBar.visibility = View.GONE
+                        mTotalPage = totalPage
+                        mIsLoadFirstTimeSuccess = true
+                    }
+                }
+                mTextMessage.visibility = View.GONE
+                mCurrentPage++
+            }
+
+            override fun onNotFound() {
+                when (mIsLoadFirstTimeSuccess) {
+                    true -> {
+                        mCurrentPage = 1
+                        mUserList.clear()
+                        mUserAdapter.notifyDataSetChanged()
+                    }
+                    false -> mProgressBar.visibility = View.GONE
+                }
+                mTextMessage.visibility = View.VISIBLE
+                mTextMessage.text = R.string.empty_review_like.toString()
+            }
+
+            override fun onError(message: String) {
+                when (mIsLoadFirstTimeSuccess) {
+                    true -> Snackbar.make(mAnchorLayout, message, Snackbar.LENGTH_LONG).show()
+                    false -> {
+                        mProgressBar.visibility = View.GONE
+                        mTextMessage.visibility = View.VISIBLE
+                        mTextMessage.text = message
+                    }
+                }
+            }
+        })
+
+        // Memberhentikan loading
+        mIsLoading = false
+        mSwipeRefresh.isRefreshing = false
+    }
+
+    private fun gotoUserDetail(id: Int) {
+        val intent = Intent(mContext, UserDetailActivity::class.java)
+        intent.putExtra(Popularin.USER_ID, id)
+        startActivity(intent)
     }
 }
