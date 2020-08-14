697a18329384a74246ea17f5ed73c873f0a97ca4
Rommy Gustiawan <rgcppp@gmail.com>
2020-08-14 22:54:59 +0700

    Finalisasi logic dialog watch date picker

--- app/src/main/java/xyz/fairportstudios/popularin/dialogs/WatchDatePickerDialog.kt
+++ app/src/main/java/xyz/fairportstudios/popularin/dialogs/WatchDatePickerDialog.kt
@@ -2,7 +2,6 @@ package xyz.fairportstudios.popularin.dialogs
 
 import android.app.DatePickerDialog
 import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import java.util.*
@@ -10,13 +9,13 @@ import java.util.*
 class WatchDatePickerDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val year = calendar[Calendar.YEAR]
+        val month = calendar[Calendar.MONTH]
+        val day = calendar[Calendar.DAY_OF_MONTH]
 
         val datePickerDialog = DatePickerDialog(
             requireActivity(),
-            activity as DatePickerDialog.OnDateSetListener,
+            requireActivity() as DatePickerDialog.OnDateSetListener,
             year,
             month,
             day
