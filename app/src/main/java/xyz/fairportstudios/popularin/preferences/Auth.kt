2cb866a51e9c04339b81ca5b88e6c75f44cd6a75
Rommy Gustiawan <rgcppp@gmail.com>
2020-08-14 21:49:36 +0700

    Finalisasi logic class auth

--- app/src/main/java/xyz/fairportstudios/popularin/preferences/Auth.kt
+++ app/src/main/java/xyz/fairportstudios/popularin/preferences/Auth.kt
@@ -1,38 +1,39 @@
 package xyz.fairportstudios.popularin.preferences
 
 import android.content.Context
-import android.content.SharedPreferences
 import xyz.fairportstudios.popularin.statics.Popularin
 
-class Auth(private val context: Context) {
+class Auth(context: Context) {
+    // Variable member
+    private val mSharedPreferences = context.getSharedPreferences(Popularin.AUTH, Context.MODE_PRIVATE)
+
     fun isAuth(): Boolean {
-        val sharedPrefs: SharedPreferences = context.getSharedPreferences(Popularin.AUTH, Context.MODE_PRIVATE)
-        val authID: Int = sharedPrefs.getInt(Popularin.AUTH_ID, 0)
-        val authToken: String? = sharedPrefs.getString(Popularin.AUTH_TOKEN, "")
+        val authID = mSharedPreferences.getInt(Popularin.AUTH_ID, 0)
+        val authToken = mSharedPreferences.getString(Popularin.AUTH_TOKEN, "")
         return authID != 0 && authToken != ""
     }
 
+    fun isSelf(userID: Int, authID: Int): Boolean {
+        return userID == authID
+    }
+
     fun getAuthID(): Int {
-        val sharedPrefs: SharedPreferences = context.getSharedPreferences(Popularin.AUTH, Context.MODE_PRIVATE)
-        return sharedPrefs.getInt(Popularin.AUTH_ID, 0)
+        return mSharedPreferences.getInt(Popularin.AUTH_ID, 0)
     }
 
     fun getAuthToken(): String? {
-        val sharedPrefs: SharedPreferences = context.getSharedPreferences(Popularin.AUTH, Context.MODE_PRIVATE)
-        return sharedPrefs.getString(Popularin.AUTH_TOKEN, "")
+        return mSharedPreferences.getString(Popularin.AUTH_TOKEN, "")
     }
 
     fun setAuth(authID: Int, authToken: String) {
-        val sharedPrefs: SharedPreferences = context.getSharedPreferences(Popularin.AUTH, Context.MODE_PRIVATE)
-        val editor: SharedPreferences.Editor = sharedPrefs.edit()
+        val editor = mSharedPreferences.edit()
         editor.putInt(Popularin.AUTH_ID, authID)
         editor.putString(Popularin.AUTH_TOKEN, authToken)
         editor.apply()
     }
 
     fun delAuh() {
-        val sharedPrefs: SharedPreferences = context.getSharedPreferences(Popularin.AUTH, Context.MODE_PRIVATE)
-        val editor: SharedPreferences.Editor = sharedPrefs.edit()
+        val editor = mSharedPreferences.edit()
         editor.clear()
         editor.apply()
     }
