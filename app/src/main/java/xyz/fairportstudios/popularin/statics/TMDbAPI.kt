44b71a985c31696de20d30053843f20ae95d65ff
Rommy Gustiawan <rgcppp@gmail.com>
2020-08-14 21:10:33 +0700

    Finalisasi logic class package statics

--- app/src/main/java/xyz/fairportstudios/popularin/statics/TMDbAPI.kt
+++ app/src/main/java/xyz/fairportstudios/popularin/statics/TMDbAPI.kt
@@ -2,13 +2,13 @@ package xyz.fairportstudios.popularin.statics
 
 class TMDbAPI {
     companion object {
-        private const val BASE_URL: String = "https://api.themoviedb.org/3"
-        const val BASE_SMALL_IMAGE_URL: String = "https://image.tmdb.org/t/p/w154"
-        const val BASE_LARGE_IMAGE_URL: String = "https://image.tmdb.org/t/p/w780"
-        const val AIRING: String = "$BASE_URL/movie/now_playing"
-        const val CREDIT: String = "$BASE_URL/person/"
-        const val DISCOVER: String = "$BASE_URL/discover/movie"
-        const val FILM: String = "$BASE_URL/movie/"
-        const val SEARCH_FILM: String = "$BASE_URL/search/movie"
+        private const val BASE_URL = "https://api.themoviedb.org/3"
+        const val BASE_SMALL_IMAGE_URL = "https://image.tmdb.org/t/p/w154"
+        const val BASE_LARGE_IMAGE_URL = "https://image.tmdb.org/t/p/w780"
+        const val AIRING = "$BASE_URL/movie/now_playing"
+        const val CREDIT = "$BASE_URL/person/"
+        const val DISCOVER = "$BASE_URL/discover/movie"
+        const val FILM = "$BASE_URL/movie/"
+        const val SEARCH_FILM = "$BASE_URL/search/movie"
     }
 }
