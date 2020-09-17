17bb16634b64edfb0ef868f04193b7bfadd7657b
Rommy Gustiawan <rgcppp@gmail.com>
2020-09-17 14:52:11 +0700

    UPDATE: Menambahkan properties dan mengubah tipe properties

--- app/src/main/java/xyz/fairportstudios/popularin/models/FilmDetail.kt
+++ app/src/main/java/xyz/fairportstudios/popularin/models/FilmDetail.kt
@@ -3,6 +3,9 @@ package xyz.fairportstudios.popularin.models
 data class FilmDetail(
     val genreID: Int,
     val runtime: Int,
+    val hasOverview: Boolean,
+    val hasCast: Boolean,
+    val hasCrew: Boolean,
     val originalTitle: String,
     val releaseDate: String,
     val overview: String,
--- app/src/main/java/xyz/fairportstudios/popularin/models/UserDetail.kt
+++ app/src/main/java/xyz/fairportstudios/popularin/models/UserDetail.kt
@@ -1,12 +1,15 @@
 package xyz.fairportstudios.popularin.models
 
 data class UserDetail(
+    val isSelf: Boolean,
     val isFollower: Boolean,
-    val isFollowing: Boolean,
+    var isFollowing: Boolean,
+    val hasRecentFavorite: Boolean,
+    val hasRecentReview: Boolean,
     val totalReview: Int,
     val totalFavorite: Int,
     val totalWatchlist: Int,
-    val totalFollower: Int,
+    var totalFollower: Int,
     val totalFollowing: Int,
     val fullName: String,
     val username: String,
